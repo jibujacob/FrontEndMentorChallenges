790be871e815b1a9d30d6cc6d6f4745aecddc4cf
jibujacob <jibupjacob@gmail.com>
2022-01-19 11:53:24 +0530

    topbar update ,git hub link adition

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# FrontEndMentorChallenges
+# FrontEndMentor Challenges
+
+    This website is built to collate all the frontend Mentor challenges I have completed as part of skill refinement and design implementations practice
+
+## Framework/Packages Used
+
+    1. React
+    2. Styled Components
+    3. React Router Dom
+
+## CI/CD
+
+    1. Using Docker and K8 during development for CI/CD readiness
+    2. Utilizing skaffold to have continous development
+
+## Future Implementation
+
+    Integrating with Git Actions for continous testing and deployment
--- client/src/components/topbar.jsx
+++ client/src/components/topbar.jsx
@@ -8,14 +8,29 @@ const Container = styled.div`
     top:0;
     display:flex;
     align-items:center;
-    justify-content:center;
+    justify-content:space-between;
     font-family: 'Manrope', sans-serif;
     background-color:#d3e1e3;
     z-index:999;
+    padding:50px;
+
+    @media only screen and (max-width:450px){
+        padding:0;
+    }
+
 `
 
 const Wrapper = styled.div`
-    padding:10px;
+    padding:50px;
+    display:flex;
+    width:100%;
+    align-items:center;
+    justify-content:space-between;
+
+    @media only screen and (max-width:450px){
+        padding:20px;
+    }
+
 `
 
 const Title = styled.span`
@@ -24,7 +39,19 @@ const Title = styled.span`
     margin:30px 0;
     letter-spacing:1px;
     font-size:22px;
-    text-align:center;
+
+`
+
+const Span = styled.span`
+    display:flex;
+    align-items:center;
+    justify-content:flex-end;
+    font-size:24px;
+    /* right:0;
+    position:absolute; */
+    cursor:pointer;
+    padding:10px;
+    color:black;
 `
 
 const Topbar = () => {
@@ -32,6 +59,12 @@ const Topbar = () => {
         <Container>
             <Wrapper>
                 <Title>Frontend Mentor Challenges</Title>
+                <Span><a href="https://github.com/jibujacob/FrontEndMentorChallenges" 
+                        rel="noreferrer"
+                    target="_blank">
+                        <i className="fab fa-github"></i>
+                    </a>         
+                </Span>
             </Wrapper>
         </Container>
     )
